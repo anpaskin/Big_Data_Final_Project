0fb007dced6a9127d906d2319661817963b4efe1
kaylascharfstein <32911842+kaylascharfstein@users.noreply.github.com>
2018-12-04 17:08:16 +0100

    Update track_number.py

--- track_number.py
+++ track_number.py
@@ -1,6 +1,15 @@
+import sklearn as sk
+from sklearn.svm import LinearSVC
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPClassifier
+
+# read in data from csv
 data = pd.read_csv("Data.csv", index_col=0)
+# drop release date because it is not numeric
 data = data.drop(columns = ["Album Release Date"])
 
+# replace NaNs with average sentiment
 sentiment = data["Sentiment"]
 average_sentiment = sentiment.mean()
 i = 0
@@ -9,23 +18,31 @@ for s in sentiment:
         data.set_value(i, "Sentiment", average_sentiment)
     i += 1
 
+# data with less features
 data2 = pd.concat([data["Total Tracks on Album"], data["Popularity"], data["Track Number"], data["Sentiment"]], axis=1)
 
 data2.columns = ["Total Tracks on Album", "Popularity", "Track Number", "Sentiment"]
 
+# features and labels
 X = data.drop(columns=["Track Number"])
 Y = data["Track Number"]
 
+# k folds cross validation
 kf = KFold(n_splits = 10, shuffle=True)
 kf.get_n_splits(X)
 
+# average accuracy
 score = 0.
 
+# loop through folds
 for train_index, test_index in kf.split(X):
+    # split into training and testing data
     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
     Y_train, Y_test = Y.iloc[train_index], Y.iloc[test_index]
+    # k nearest neighbors
     #n_neighbors = 70
     #clf = sk.neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
+    # neural network
     clf = MLPClassifier(solver="lbfgs", alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
     clf.fit(X_train, Y_train)
     score += clf.score(X_train, Y_train)/10
