3b2db1a7ed5282bfc29e07fc911060826fee8d0e
kaylascharfstein <32911842+kaylascharfstein@users.noreply.github.com>
2018-12-04 16:59:59 +0100

    Update reformat_data.py

--- reformat_data.py
+++ reformat_data.py
@@ -4,8 +4,10 @@ data = pd.read_csv("Data.csv")
 
 new_col = []
 
+# loop through total track numbers and track numbers and create new column with quartiles or halves
 for total_track, track_number in zip(data["Total Tracks on Album"], data["Track Number"]):
     f = float(track_number)/float(total_track)
+    # for quarters
     # if f >= 0 and f < .25:
     #     new_col.append(1)
     # elif f >= .25 and f < .5:
@@ -14,11 +16,15 @@ for total_track, track_number in zip(data["Total Tracks on Album"], data["Track 
     #     new_col.append(3)
     # else:
     #     new_col.append(4)
+    # for halves
     if f >= 0 and f < .5:
         new_col.append(0)
     else:
         new_col.append(1)
 
+# concatenate data frames and drop irrelevant features
 data = pd.concat([data, pd.DataFrame({"Half" : new_col})], axis = 1)
 data = data.drop(columns = ["Track Number", "Total Tracks on Album"])
+
+# create csv
 data.to_csv("Data3.csv")
