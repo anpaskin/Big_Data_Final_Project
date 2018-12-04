70102071be4f577df0501ee8d44c3c8f64a0e2b9
kaylascharfstein <32911842+kaylascharfstein@users.noreply.github.com>
2018-12-04 16:40:11 +0100

    Half

---
+++ half.py
@@ -0,0 +1,40 @@
+import sklearn as sk
+from sklearn.svm import LinearSVC
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPClassifier
+
+data = pd.read_csv("Data3.csv", index_col=0)
+data = data.drop(columns = ["Album Release Date"])
+
+sentiment = data["Sentiment"]
+average_sentiment = sentiment.mean()
+i = 0
+for s in sentiment:
+    if pd.isna(s):
+        data.set_value(i, "Sentiment", average_sentiment)
+    i += 1
+
+data2 = pd.concat([data["Popularity"], data["Half"], data["Sentiment"]], axis=1)
+
+data2.columns = ["Popularity", "Half", "Sentiment"]
+
+X = data.drop(columns=["Half"])
+Y = data["Half"]
+
+kf = KFold(n_splits = 10, shuffle=True)
+kf.get_n_splits(X)
+
+score = 0.
+
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    Y_train, Y_test = Y.iloc[train_index], Y.iloc[test_index]
+    n_neighbors = 70
+    clf = sk.neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
+    #clf = MLPClassifier(solver="lbfgs", alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    clf.fit(X_train, Y_train)
+    score += clf.score(X_train, Y_train)/10
+
+print score
+    
