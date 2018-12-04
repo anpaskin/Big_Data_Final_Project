db331e2f9631bf2cfe1ec9bca3cc66250b3d7f28
kaylascharfstein <32911842+kaylascharfstein@users.noreply.github.com>
2018-12-04 17:05:48 +0100

    Update request.py

--- request.py
+++ request.py
@@ -5,8 +5,8 @@ import csv
 from get_lyrics import sentiment
 
 albums = set()
-artist_set = set()
 
+# create a csv file and write headers to it
 ofile = open('Test6.csv', "wb")
 writer = csv.writer(ofile, quotechar='"', quoting=csv.QUOTE_ALL)
 writer.writerow([
@@ -33,26 +33,41 @@ writer.writerow([
     "Tempo"
 ])
 
+# write songs from playlist to csv
 def print_playlist(sp, playlist):
+    # get playlists in a category
     results = sp.category_playlists(playlist)
     items = results['playlists']['items']
-
+    
+    # loop through playlists in a category
     for item in items:
+        # owner of playlist
         owner = item['owner']['display_name']
+        # id of playlist
         id = item['id']
+        # tracks in playlist
         tracks = sp.user_playlist_tracks(owner, id)['items']
+        
+        # loop through tracks
         for track in tracks:
             try:
+                # get only albums that have not been seen before
                 if track['track']['album']['album_type'] == "album" and track['track']['album']['id'] not in albums:
+                    # get all tracks on album
                     album_tracks = sp.album_tracks(track['track']['album']['id'])['items']
+                    
+                    # loop through tracks on album
                     for album_track in album_tracks:
+                        # get version of track that has more features available
                         t = sp.track(album_track['uri'])
+                        # get audio features of track
                         audioFeatures = sp.audio_features([album_track['uri']])
+                        # get artists for track
                         artists = ""
                         try:
                             for artist in t['artists']:
-                                artist_set.add(artist['name'].encode('utf-8'))
                                 artists = artists + artist['name'].encode('utf-8') + ','
+                            # write features to csv
                             writer.writerow([
                                 t['name'].encode('utf-8'),
                                 t['album']['name'].encode('utf-8'),
@@ -79,12 +94,10 @@ def print_playlist(sp, playlist):
                         except UnicodeDecodeError:
                             print "Unicode Decode Error"
                             pass
+                    # add album id to albums
                     albums.add(track['track']['album']['id'])
             except TypeError:
                 pass
-    with open(playlist + ".txt", "w") as f:
-        for artist in artist_set:
-            f.write(artist)
 
 scope = 'user-library-read'
 client_id = '7c9b11cd0657484a91f87d8dd67fb70d'
@@ -97,10 +110,13 @@ else:
     print "Usage: %s username" % (sys.argv[0],)
     sys.exit()
 
+# api token
 token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
 if token:
+    # create instance of spotify
     sp = spotipy.Spotify(auth=token)
+    # call print playlist on country category
     print_playlist(sp, "country")
 else:
     print "Can't get token for", username
